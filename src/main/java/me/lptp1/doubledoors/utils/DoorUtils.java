00bf5bbefad7d67aabf0379910ac5d90a2b7ba7e
LPTP1Dev <51876648+LPTP1Dev@users.noreply.github.com>
2024-02-16 16:28:57 +0100

    Ignore iron doors
    they should not be opened by hand

--- src/main/java/me/lptp1/doubledoors/utils/DoorUtils.java
+++ src/main/java/me/lptp1/doubledoors/utils/DoorUtils.java
@@ -6,6 +6,7 @@ import org.bukkit.block.data.type.Door;
 public class DoorUtils {
     public static boolean isDoorBlock(Block block) {
         if (block == null) return false;
+        if (block.getType().toString().contains("IRON")) return false; // We don't want to open iron doors by hand
         return block.getType().toString().contains("DOOR") && !block.getType().toString().contains("TRAP");
     }
 
